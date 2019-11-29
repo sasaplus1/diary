2d1d402f45c6cc46d234c44547b53c168e55964a
sasaplus1 <sasaplus1@gmail.com>
2019-11-29 17:35:30 +0900

    prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "*.css": "npx stylelint --cache",
-  "*.js": "npx eslint --cache",
-  "package.json": [
-    "npx fixpack",
-    "npx prettier --parser json-stringify --write",
-    "git diff --exit-code --quiet"
+  '*.css': 'npx stylelint --cache',
+  '*.js': 'npx eslint --cache',
+  'package.json': [
+    'npx fixpack',
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
   ],
-  "package-lock.json": 'node -e "process.exitCode = 1;"'
+  'package-lock.json': 'node -e "process.exitCode = 1;"'
 };
